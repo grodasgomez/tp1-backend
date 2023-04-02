57611d8c33c5480df85e08d2248d587b30f81efc
Guillermo <guigapamplona@gmail.com>
2023-04-01 20:07:03 -0400

    Add model to handle point rules

--- src/main/java/py/com/progweb/api/ejb/PointRuleDAO.java
+++ src/main/java/py/com/progweb/api/ejb/PointRuleDAO.java
@@ -1,2 +1,64 @@
-package py.com.progweb.api.ejb;public class PointRuleDAO {
+package py.com.progweb.api.ejb;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import py.com.progweb.api.exceptions.ApiException;
+import py.com.progweb.api.model.PointRule;
+
+import java.util.List;
+
+@Stateless
+public class PointRuleDAO {
+    @PersistenceContext(unitName = "pruebaPU")
+    private EntityManager em;
+
+    public List<PointRule> getAll() {
+        return this.em.createQuery("select c from PointRule c", PointRule.class).getResultList();
+    }
+
+    public PointRule getById(Integer id) {
+        return this.em.find(PointRule.class, id);
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public PointRule create(PointRule pointRule) {
+        pointRule.setId(null);
+        this.em.persist(pointRule);
+        return pointRule;
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public PointRule delete(Integer id) throws ApiException {
+        PointRule pointRule = this.getById(id);
+        if (pointRule == null) {
+            throw new ApiException("Point Rule not found", 404);
+        }
+        this.em.remove(pointRule);
+        return pointRule;
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public PointRule update(Integer id, PointRule payload) throws ApiException {
+        PointRule pointRule = this.getById(id);
+        if (pointRule == null) {
+            throw new ApiException("Point Rule not found", 404);
+        }
+        if (payload.getLowerLimit() != null) {
+            pointRule.setLowerLimit(payload.getLowerLimit());
+        }
+        if (payload.getUpperLimit() != null) {
+            pointRule.setUpperLimit(payload.getUpperLimit());
+        }
+        if (payload.getConversionRate() != null) {
+            pointRule.setConversionRate(payload.getConversionRate());
+        }
+
+        this.em.merge(pointRule);
+
+        return pointRule;
+    }
 }
--- src/main/java/py/com/progweb/api/model/PointRule.java
+++ src/main/java/py/com/progweb/api/model/PointRule.java
@@ -1,12 +1,31 @@
 package py.com.progweb.api.model;
 
-import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 @Entity
-@Table(name = "rule")
-public class Rule {
-    
+@Table(name = "point_rule")
+@Getter
+@Setter
+public class PointRule {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "pointRuleSec")
+    @SequenceGenerator(name = "pointRuleSec", sequenceName = "point_rule_sec", allocationSize = 0)
+    private Integer id;
+
+    @Column(name = "lower_limit", nullable = false)
+    private Integer lowerLimit;
+
+    @Column(name = "upper_limit", nullable = false)
+    private Integer upperLimit;
+
+    @Column(name = "conversion_rate", nullable = false)
+    private Integer conversionRate;
+
+    public PointRule() {
+
+    }
 }
